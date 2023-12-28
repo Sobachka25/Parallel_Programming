f601899de5d6005b346611a405ca37e595b35680
Sobachka25 <akrug555@yandex.ru>
2023-12-28 03:53:26 +0300

    The firs and the last

---
+++ MPI/MPI/MPI.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include "mpi.h"
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    srand(time(NULL) + rank);
+
+    // определяем размеры матриц
+    const int M = 3000;
+    const int N = 3000;
+    const int T = 3000;
+
+    // вычисляем количество строк и столбцов для каждого процесса
+    const int rows_per_proc = M / size;
+    const int cols_per_proc = T / size;
+
+    // создаем матрицы на каждом процессе
+    int* A = new int[rows_per_proc * N];
+    int* B = new int[N * cols_per_proc];
+    int* C = new int[rows_per_proc * cols_per_proc];
+
+    // заполняем матрицы случайными числами
+    for (int i = 0; i < rows_per_proc; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i * N + j] = rand() % 10;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < cols_per_proc; j++) {
+            B[i * cols_per_proc + j] = rand() % 10;
+        }
+    }
+
+    // перемножаем матрицы
+    unsigned int start_time = clock();
+    for (int i = 0; i < rows_per_proc; i++) {
+        for (int j = 0; j < cols_per_proc; j++) {
+            C[i * cols_per_proc + j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i * cols_per_proc + j] += A[i * N + k] * B[k * cols_per_proc + j];
+            }
+        }
+    }
+
+    // собираем результаты на процессе 0
+    if (rank == 0) {
+        int* result = new int[M * T];
+        for (int i = 0; i < rows_per_proc; i++) {
+            for (int j = 0; j < cols_per_proc; j++) {
+                result[i * T + j] = C[i * cols_per_proc + j];
+            }
+        }
+
+        for (int p = 1; p < size; p++) {
+            MPI_Recv(C, rows_per_proc * cols_per_proc, MPI_INT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            for (int i = 0; i < rows_per_proc; i++) {
+                for (int j = 0; j < cols_per_proc; j++) {
+                    result[p * rows_per_proc * cols_per_proc + i * T + j] = C[i * cols_per_proc + j];
+                }
+            }
+        }
+        unsigned int end_time = clock(); // конечное время
+        unsigned int search_time = end_time - start_time;
+        cout << "vremya vipolneniya na " << " potokah: " << search_time << " ";
+        cout << endl;
+
+        // выводим результаты
+        /*for (int i = 0; i < M; i++) {
+            for (int j = 0; j < T; j++) {
+                cout << result[i * T + j] << " ";
+            }
+            cout << endl;
+        }*/
+
+        delete[] result;
+    }
+    else {
+        MPI_Send(C, rows_per_proc * cols_per_proc, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+
+    // освобождаем память
+    delete[] A;
+    delete[] B;
+    delete[] C;
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ OpenMP/OpenMP/OpenMP.cpp
@@ -0,0 +1,70 @@
+﻿// OpenMP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <omp.h>
+#include <stdio.h>
+#include <ctime>
+
+using namespace std;
+
+void multiply_matrices(int** A, int** B, int** C, int M, int N, int T, int th) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+#pragma omp parallel for shared(A, B, C) private(i, j, k) num_threads(12)
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < T; j++) {
+            C[i][j] = 0;
+            for (k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    const int M = 2000;
+    const int N = 2000;
+    const int T = 2000;
+
+    int** A = new int* [M];
+    int** B = new int* [N];
+    int** C = new int* [M];
+
+    for (int i = 0; i < M; i++) {
+        A[i] = new int[N];
+        C[i] = new int[T];
+        for (int j = 0; j < N; j++) {
+            A[i][j] = rand() % 10;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        B[i] = new int[T];
+        for (int j = 0; j < T; j++) {
+            B[i][j] = rand() % 10;
+        }
+    }
+    for (int th = 1; th < 13; th++) {
+        unsigned int start_time = clock(); // начальное время
+        multiply_matrices(A, B, C, M, N, T, th);
+        unsigned int end_time = clock(); // конечное время
+        unsigned int search_time = end_time - start_time;
+        cout << "vremya vipolneniya na "<<th<< " potokah: " << search_time << " ";
+        cout << endl;
+    }
+    // Освобождение памяти
+    for (int i = 0; i < M; i++) {
+        delete[] A[i];
+        delete[] C[i];
+    }
+    for (int i = 0; i < N; i++) {
+        delete[] B[i];
+    }
+    delete[] A;
+    delete[] B;
+    delete[] C;
+
+    return 0;
+}
